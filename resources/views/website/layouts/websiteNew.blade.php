3dce7ad75ace229d367c10acf6cd015cd58f338f
ArSalanAyoub <arsalanayoub48@gmail.com>
2022-06-21 01:06:25 +0500

    bug fixed for mobile view menu

--- resources/views/website/layouts/websiteNew.blade.php
+++ resources/views/website/layouts/websiteNew.blade.php
@@ -218,7 +218,6 @@
                                 <?php } ?>
                             </div>
                         </div>
-                        </div>
                     @endforeach
                 </div>
             </div>
