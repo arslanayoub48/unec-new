fbd4195ac80875fa6c6da8f631abd000594c3426
pektiyaz <pektiyaz@mail.ru>
2022-06-28 09:05:00 +0400

    test

--- resources/views/website/layouts/website.blade.php
+++ resources/views/website/layouts/website.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!--Title Here-->
-    <title>Azərbaycan Dövlət İqtisad Universiteti</title>
+    <title>2 Azərbaycan Dövlət İqtisad Universiteti</title>
     <!--Stylesheets-->
     <link href="/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
